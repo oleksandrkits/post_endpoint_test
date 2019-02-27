6136f41849ee8a21970ae08d6782a2fd56e10f24
Oleksandr Kitsera <oleksandrkit@interlink-ua.com>
2019-02-27 17:00:51 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,17 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({
+    extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post('/callback', function (req, res, next) {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+let port = 4001;
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
+});
